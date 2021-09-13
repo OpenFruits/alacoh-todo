d07306515529d356f28477a9b6f376589f1b33f5
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-09-13 20:41:51 +0900

    ✨ Add submit button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
-import type { NextPage } from 'next'
-import { Layout } from 'src/components/layout';
-import { styled } from '../styles/stitches.config';
+import type { NextPage } from "next";
+import { useCallback, useState } from "react";
+import { Layout } from "src/components/layout";
+import { styled } from "../styles/stitches.config";
 
-const Headline = styled('h2', {
-  color: "$teal11"
-})
+const Headline = styled("h2", {
+  color: "$teal11",
+});
 
 const Home: NextPage = () => {
+  const [text, setText] = useState("");
+  const inputText = useCallback((e) => setText(e.target.value), [setText]);
+  console.log(text);
+
+  const submit = () => {
+    setText("");
+  };
+
   return (
     <Layout>
       <div>
         <Headline>INPUT</Headline>
-        <input type="text" />
+        <input type="text" onChange={inputText} />
+        <button onChange={submit}>Add</button>
       </div>
       <div>
         <Headline>TODAY&apos;S TODO</Headline>
@@ -21,7 +31,7 @@ const Home: NextPage = () => {
           <li>task3</li>
         </ol>
       </div>
-      <div>      
+      <div>
         <Headline>NEXT DAY</Headline>
         <ul>
           <li>task1</li>
@@ -38,7 +48,7 @@ const Home: NextPage = () => {
         </ul>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
